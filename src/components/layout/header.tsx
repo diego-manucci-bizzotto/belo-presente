60caa0c70871f3513bf42795a2924c15a96fa756
diego <diegobizzotto2003@gmail.com>
2025-07-25 16:25:27 -0300

    Refactor: Remove rounded style

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ import {logout} from "@/lib/firebase/auth";
 
 export default function Header() {
   return (
-    <header className="flex items-center p-4 bg-background text-white rounded-b-lg border w-full mx-auto">
+    <header className="flex items-center p-4 bg-background text-white border w-full mx-auto">
       <Image src="/images/logo.svg" alt="logo" width={1024} height={1024} className="w-12 h-auto"/>
       <h1 className="text-3xl font-bold ml-4 text-[#b1563c]">Belo Presente</h1>
       <Button onClick={logout}>
