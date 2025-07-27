7ccf394e7fa20e2b56aa156c6b68cb78bc9ab370
diego <diegobizzotto2003@gmail.com>
2025-07-27 17:51:08 -0300

    Refactor: Change side-nav size and remove back button

--- src/app/(protected)/lists/[listId]/layout.tsx
+++ src/app/(protected)/lists/[listId]/layout.tsx
@@ -1,17 +1,13 @@
 "use client"
 
 import React, {use} from "react";
-import {getEmojiByCategory} from "@/utils/utils";
 import Link from "next/link";
 import {Gift, Image, Link as LinkIcon, MessagesSquare, Palette, Settings, Users} from "lucide-react";
 import {cn} from "@/lib/utils";
 import {notFound, usePathname} from "next/navigation";
 import {useGetList} from "@/services/lists/getList";
 
-export default function RootLayout({children, params}: Readonly<{
-  children: React.ReactNode,
-  params: Promise<{ listId: string }>
-}>) {
+export default function RootLayout({children, params}: Readonly<{ children: React.ReactNode, params: Promise<{ listId: string }> }>) {
   const pathname = usePathname();
 
   const {listId} = use(params);
@@ -23,16 +19,16 @@ export default function RootLayout({children, params}: Readonly<{
   const lastPathSegment = pathname.split("/").pop();
 
   return (
-    <main className="flex flex-col gap-4 flex-grow p-4 justify-center items-center">
+    <main className="flex flex-col items-start gap-4 flex-grow p-4 justify-center items-center">
       <div className='w-full'>
-        <h1 className="text-2xl font-bold">{getEmojiByCategory(list.data?.category || "")} {list.data?.title}</h1>
+        <h1 className="text-2xl font-bold">{list.data?.title}</h1>
         <p className='text-muted-foreground'>
           Esta lista está pausada no momento.
           Para que seus convidados possam acessá-la, vá até as configurações e mude o status para pública.
         </p>
       </div>
-      <div className='w-full flex p-4'>
-        <div className='min-w-xs flex flex-col gap-2'>
+      <div className='w-full flex p-4 gap-8'>
+        <div className='min-w-[200px] flex flex-col gap-2'>
           <span className='text-muted-foreground'>Funcionalidades</span>
           <ul>
             <li>
