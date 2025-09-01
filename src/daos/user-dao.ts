1dfcb9bfa21c709fd16e6b9c8086574244338ba6
diego <diegobizzotto2003@gmail.com>
2025-09-01 20:20:12 -0300

    Refactor: Change db schema naming convention

--- src/daos/user-dao.ts
+++ src/daos/user-dao.ts
@@ -2,35 +2,41 @@ import { Database } from "@/lib/pg/database";
 import { PoolClient } from "pg";
 
 export class UserDAO {
-
   private constructor() {}
 
   public static async findUserByEmail(email: string, client?: PoolClient) {
     const db = Database.getInstance();
     const runner = client || db;
     const { rows } = await runner.query(
-      "SELECT * FROM users WHERE email = $1",
+      'SELECT * FROM "user" WHERE email = $1',
       [email]
     );
     return rows[0];
   }
 
-  public static async createUser({ email, passwordHash }: { email: string; passwordHash: string }, client?: PoolClient) {
+  public static async createUser(
+    { email, passwordHash }: { email: string; passwordHash: string },
+    client?: PoolClient
+  ) {
     const db = Database.getInstance();
     const runner = client || db;
     const { rows } = await runner.query(
-      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email",
+      'INSERT INTO "user" (email, password_hash) VALUES ($1, $2) RETURNING id, email',
       [email, passwordHash]
     );
     return rows[0];
   }
 
-  public static async updateUserPassword(userId: string, passwordHash: string, client?: PoolClient) {
+  public static async updateUserPassword(
+    userId: string,
+    passwordHash: string,
+    client?: PoolClient
+  ) {
     const db = Database.getInstance();
     const runner = client || db;
     await runner.query(
-      "UPDATE users SET password_hash=$1 WHERE id=$2",
-      [passwordHash, userId,]
+      'UPDATE "user" SET password_hash=$1 WHERE id=$2',
+      [passwordHash, userId]
     );
   }
 }
