e255d7ab25304c113bd6e85ff5d697ba0961342a
diego <diegobizzotto2003@gmail.com>
2025-09-10 20:12:55 -0300

    Refactor: Change the style of the mobile route select

--- src/components/app/lists/[listId]/list-header.tsx
+++ src/components/app/lists/[listId]/list-header.tsx
@@ -2,9 +2,18 @@
 
 import { useRouter } from "next/navigation";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
 import { Banknote, Gift, Image, Link as LinkIcon, MessagesSquare, Palette, Settings, Users } from "lucide-react";
 import { useGetList } from "@/hooks/use-get-list";
+import {cn} from "@/lib/utils";
 
 const FEATURES = [
   { value: "products", label: "Presentes", icon: Gift },
@@ -28,7 +37,6 @@ export function ListHeader({ listId, pathname }: ListHeaderProps) {
   const lastPathSegment = pathname.split("/").pop();
 
   const handleNavigate = (value: string) => {
-
     router.push(`/lists/${listId}/${value}`);
   }
 
@@ -40,24 +48,31 @@ export function ListHeader({ listId, pathname }: ListHeaderProps) {
         ) : (
           <h1 className="text-2xl font-bold">{list?.title}</h1>
         )}
-        <div className="md:hidden">
-          <Select
-            value={lastPathSegment}
-            onValueChange={handleNavigate}
-          >
-            <SelectTrigger className="w-full min-w-40">
-              <SelectValue placeholder="Selecione uma funcionalidade" />
-            </SelectTrigger>
-            <SelectContent>
-              {FEATURES.map(f => (
-                <SelectItem key={f.value} value={f.value}>
-                  <f.icon className="inline-block mr-2 h-4 w-4" />
-                  {f.label}
+        <Select value={lastPathSegment} onValueChange={handleNavigate}>
+          <SelectTrigger className="w-full w-auto hover:cursor-pointer text-primary md:hidden">
+            <SelectValue placeholder="Selecione" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Funcionalidades</SelectLabel>
+              {FEATURES.slice(0, 4).map(feature => (
+                <SelectItem key={feature.value} value={feature.value} className={cn('hover:cursor-pointer text-muted-foreground hover:text-black', feature.value.includes(lastPathSegment || "") && "text-primary! hover:text-primary! hover:cursor-default bg-white! hover:bg-white!")}>
+                <feature.icon className={cn("inline-block mr-2 h-4 w-4 hover:text-black", feature.value.includes(lastPathSegment || "") && "text-primary")} />
+                  {feature.label}
                 </SelectItem>
               ))}
-            </SelectContent>
-          </Select>
-        </div>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Gestão</SelectLabel>
+              {FEATURES.slice(4, 8).map(feature => (
+                <SelectItem key={feature.value} value={feature.value} className={cn('hover:cursor-pointer text-muted-foreground hover:text-black', feature.value.includes(lastPathSegment || "") && "text-primary")}>
+                  <feature.icon className={cn("inline-block mr-2 h-4 w-4 hover:text-black", feature.value.includes(lastPathSegment || "") && "text-primary")}/>
+                  {feature.label}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
       {!isLoading && !isPending && !list?.active && (
         <p className='text-muted-foreground text-sm'>
